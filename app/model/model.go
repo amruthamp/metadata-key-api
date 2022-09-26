ca30bd1e45fa4d3e7f19881dc4c56c3a84924443
“amruthamp” <mpamrutha96@gmail.com>
2022-09-26 11:40:48 +0530

    removed possibe values from metadata attribute api

--- app/model/model.go
+++ app/model/model.go
@@ -1,10 +1,9 @@
 package model
 
 type Attribute struct {
-	Name            string   `json:"name"`
-	IsMultipleField string   `json:"isMultipleField"`
-	IsMandatory     string   `json:"isMandatory"`
-	DataType        string   `json:"dataType"`
-	PossibleValues  []string `json:"possibleValues"`
-	Id              string   `json:"id"`
+	Name            string `json:"name"`
+	IsMultipleField string `json:"isMultipleField"`
+	IsMandatory     string `json:"isMandatory"`
+	DataType        string `json:"dataType"`
+	Id              string `json:"id"`
 }
